dcc66fe5272c1f12401669ebaef45c26f3595854
Hideo Hattori <hhatto.jp@gmail.com>
2020-06-29 21:21:44 +0900

    transfer mercurial to git

---
+++ test/python2/less.py
@@ -0,0 +1,66 @@
+import pprint
+import hotshot, hotshot.stats
+
+prof = hotshot.Profile('less.hotprof')
+
+def func0():
+    func1()
+    func2()
+    func3()
+    func4()
+    func5()
+    func6()
+    func7()
+    func8()
+    func9()
+    func10()
+    func11()
+    func12()
+    func13()
+    func14()
+    func15()
+    func16()
+    func17()
+    func18()
+def func1():
+    a = [i for i in range(5)]
+def func2():
+    a = [i for i in range(5)]
+def func3():
+    a = [i for i in range(5)]
+def func4():
+    a = [i for i in range(5)]
+def func4():
+    a = [i for i in range(5)]
+def func5():
+    a = [i for i in range(5)]
+def func6():
+    a = [i for i in range(5)]
+def func7():
+    a = [i for i in range(5)]
+def func8():
+    a = [i for i in range(5)]
+def func9():
+    a = [i for i in range(5)]
+def func10():
+    a = [i for i in range(5)]
+def func11():
+    a = [i for i in range(5)]
+def func12():
+    a = [i for i in range(5)]
+def func13():
+    a = [i for i in range(5)]
+def func14():
+    a = [i for i in range(5)]
+def func15():
+    a = [i for i in range(5)]
+def func16():
+    a = [i for i in range(5)]
+def func17():
+    a = [i for i in range(5)]
+def func18():
+    a = [i for i in range(5)]
+def func19():
+    a = [i for i in range(5)]
+
+prof.runcall(func0)
